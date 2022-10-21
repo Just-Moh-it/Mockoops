fcab6964bb2f4a882a01576881b71ff5e9872930
Mohit Yadav <www.mohit2004@gmail.com>
2022-10-22 00:45:54 +0530

    😕 chore: add warning of custom upload unavilability

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,11 +1,23 @@
 import Layout from "components/App/Layout";
 import RenderHandler from "lib/handleRender";
-import Head from 'next/head'
+import Head from "next/head";
+import { toast } from "react-hot-toast";
 
 const CreatePage = () => {
+  toast.error(
+    "Rendering videos with custom video/audio is currently unavilable (though previewing is available). Please check back in a few days, apologies.",
+    {
+      duration: 10000,
+      id: "upload-unavailable-reason",
+      position: "bottom-left",
+    }
+  );
+
   return (
     <>
-    <Head><title>Create | Mockoops</title></Head>
+      <Head>
+        <title>Create | Mockoops</title>
+      </Head>
       <Layout />
       <RenderHandler />
     </>
